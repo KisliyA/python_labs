aa8556f1da652d970257f1e51e344de8e1b63b2c
KisliyA <alexkisel567@gmail.com>
2025-09-26 11:19:35 +0300

    Task5

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,7 +1,7 @@
 full_name = input()
 cleaned_name = ' '.join(full_name.split())
 x = full_name.split()
-print(f'ФИО:{cleaned_name}')
-print(f'Инициалы:{x[0][0]}{x[1][0]}{x[2][0]}.')
-print(f'Длина (символов):{len(cleaned_name)}')
+print(f'ФИО: {cleaned_name}')
+print(f'Инициалы: {x[0][0]}{x[1][0]}{x[2][0]}.')
+print(f'Длина (символов): {len(cleaned_name)}')
 
